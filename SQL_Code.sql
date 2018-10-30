f407f7016b59c332b7094034a3b9dab94631ab15
biglala89 <chowli1103@gmail.com>
2018-10-30 01:39:59 -0700

    Capitalize Key Words

--- SQL_Code.sql
+++ SQL_Code.sql
@@ -1,61 +1,61 @@
 # Nested CTE to determine good and bad salesmen
-with total_items as (
-  select salesman_id, 
+WITH total_items AS (
+  SELECT salesman_id, 
           city_id, 
-          sum(amount_earned) as earned_amt,
-          sum(items_sold) as total_items_sold
-  from daily_sales ds
-  join salesman s
-  on ds.salesman_id = s.id
-  join city c
-  on s.city_id = c.id
-  group by salesman_id, city_id
+          SUM(amount_earned) AS earned_amt,
+          SUM(items_sold) AS total_items_sold
+  FROM daily_sales ds
+  JOIN salesman s
+  ON ds.salesman_id = s.id
+  JOIN city c
+  ON s.city_id = c.id
+  GROUP BY salesman_id, city_id
   ), 
   
-  city_average as (
-    select city_id, 
-            avg(earned_amt) as city_avg_earned
-    from total_items
-    group by city_id
+  city_average AS (
+    SELECT city_id, 
+            AVG(earned_amt) AS city_avg_earned
+    FROM total_items
+    GROUP BY city_id
     ),
   
-  comparison as (
-  select ca.city_id, 
+  comparison AS (
+  SELECT ca.city_id, 
           salesman_id, 
           earned_amt, 
           city_avg_earned,
           total_items_sold
-  from city_average ca
-  join total_items ti
-  on ca.city_id = ti.city_id
-  order by city_id
+  FROM city_average ca
+  JOIN total_items ti
+  ON ca.city_id = ti.city_id
+  ORDER BY city_id
     )
     
-select case when earned_amt > city_avg_earned then 'Good'
-        else 'Bad' 
-        end as label,
-        avg(total_items_sold) as average
-from comparison
-group by label
+SELECT CASE WHEN earned_amt > city_avg_earned THEN 'Good'
+        ELSE 'Bad' 
+        END AS label,
+        AVG(total_items_sold) AS average
+FROM comparison
+GROUP BY label
 
 
 # Show maximum average hours spent on a project with the client id
-with h20 as (
-  select project_id, count(distinct employee_id) as no_of_emp, sum(hours_spent) as total_hours
-  from allocation
-  group by 1
-  having sum(hours_spent) > 20
+WITH h20 AS (
+  SELECT project_id, COUNT(DISTINCT employee_id) AS no_of_emp, SUM(hours_spent) AS total_hours
+  FROM allocation
+  GROUP BY 1
+  HAVING SUM(hours_spent) > 20
   ),
   
-  project_client as (
-    select client_id, avg(no_of_emp) as avg_emp_num from h20
-    join project p
-    on p.id = h20.project_id
-    group by 1
+  project_client AS (
+    SELECT client_id, AVG(no_of_emp) AS avg_emp_num FROM h20
+    JOIN project p
+    ON p.id = h20.project_id
+    GROUP BY 1
     )
     
-    select avg_emp_num as maximal_average, client_id from project_client
-    where avg_emp_num = (
-      select max(avg_emp_num)
-      from project_client
+    SELECT avg_emp_num AS maximal_average, client_id FROM project_client
+    WHERE avg_emp_num = (
+      SELECT MAX(avg_emp_num)
+      FROM project_client
       )
